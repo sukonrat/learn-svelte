b1b397edb1817a51b4d3a51b1cb14387ae851c66
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-08-02 18:17:24 +1000

    learned basic

---
+++ learn-svelte/src/Another.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let name;
+    export let version;
+    export let speed;
+    export let website;
+</script>
+<p>
+    The { name } <br>
+    Package is: { speed } <br>
+    Download version: { version } <br>
+    Website: <a href={website} target="_blank">Learn more here</a><br>
+</p>
